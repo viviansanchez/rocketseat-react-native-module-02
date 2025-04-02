9e095bb28ee5259b1d525032c602c006e61b3941
Vivian Sanchez <vivian.sanchez.dev@gmail.com>
2025-04-02 12:26:49 -0300

    feat: implement fonts and Loading component

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
-import { Groups } from "@screens/Groups";
 import { ThemeProvider } from "styled-components";
 import theme from "@theme/index";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
+import { Loading } from "@components/Loading";
+import { Groups } from "@screens/Groups";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
