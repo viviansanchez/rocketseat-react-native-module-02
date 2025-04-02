c29cc28431ee800526d4720ff796d813413e7252
Vivian Sanchez <vivian.sanchez.dev@gmail.com>
2025-04-02 12:30:22 -0300

    feat: add status bar styling

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import {
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
+import { StatusBar } from "react-native";
 
 import { Loading } from "@components/Loading";
 import { Groups } from "@screens/Groups";
@@ -14,6 +15,11 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
