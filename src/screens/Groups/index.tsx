2a24c3454655993b664bf6a256b59488092d5e56
Vivian Sanchez <vivian.sanchez.dev@gmail.com>
2025-04-02 14:15:27 -0300

    feat: new component Highlight

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,13 @@
 import { Header } from "@components/header";
+import { Highlight } from "@components/Highlight";
+
 import { Container } from "./styles";
 
 export function Groups() {
   return (
     <Container>
       <Header />
+      <Highlight title="Turmas" subtitle="jogue com a sua turma" />
     </Container>
   );
 }
