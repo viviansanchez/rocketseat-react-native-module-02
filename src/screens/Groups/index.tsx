bce1521fdd96f1032313808bad264ce31f6f046a
Vivian Sanchez <vivian.sanchez.dev@gmail.com>
2025-04-02 14:50:12 -0300

    feat: new component GroupCard

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "@components/header";
 import { Highlight } from "@components/Highlight";
+import { GroupCard } from "@components/GroupCard";
 
 import { Container } from "./styles";
 
@@ -8,6 +9,7 @@ export function Groups() {
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
+      <GroupCard title="Galera do ignite" />
     </Container>
   );
 }
