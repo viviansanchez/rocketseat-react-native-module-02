552a51c8ec60f8fd0fed184e1597beca688423e6
Vivian Sanchez <vivian.sanchez.dev@gmail.com>
2025-05-16 12:01:50 -0300

    feat: new component Filter

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -3,6 +3,7 @@ import { Header } from "@components/header";
 import { Highlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Input } from "@components/Input";
+import { Filter } from "@components/Filter";
 
 export function Players() {
   return (
@@ -16,6 +17,8 @@ export function Players() {
         <Input placeholder="Nome da pessoa" autoCorrect={false} />
         <ButtonIcon icon="add" />
       </Form>
+
+      <Filter title="Time A" isActive />
     </Container>
   );
 }
