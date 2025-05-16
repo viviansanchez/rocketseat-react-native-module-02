417e6ef0c4c2721fe8f9309424e26e8960e43426
Vivian Sanchez <vivian.sanchez.dev@gmail.com>
2025-05-16 12:56:01 -0300

    feat: new component PlayerCard

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,11 +1,19 @@
-import { Container, Form } from "./styles";
+import { useState } from "react";
+import { FlatList } from "react-native";
 import { Header } from "@components/header";
 import { Highlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Input } from "@components/Input";
 import { Filter } from "@components/Filter";
+import { PlayerCard } from "@components/PlayerCard";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 
 export function Players() {
+  const [team, setTeam] = useState("Time A");
+  const [players, setPlayers] = useState([]);
+
   return (
     <Container>
       <Header showBackButton />
@@ -18,7 +26,40 @@ export function Players() {
         <ButtonIcon icon="add" />
       </Form>
 
-      <Filter title="Time A" isActive />
+      <HeaderList>
+        <FlatList
+          data={["Time A", "Time B"]}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+
+        <NumberOfPlayers>{players.length}</NumberOfPlayers>
+      </HeaderList>
+
+      <FlatList
+        data={players}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <PlayerCard name={item} onRemove={() => {}} />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          players.length === 0 && { flex: 1 },
+        ]}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Não há pessoas nesse time" />
+        )}
+      />
+
+      <Button title="Remover Turma" type="SECONDARY" />
     </Container>
   );
 }
