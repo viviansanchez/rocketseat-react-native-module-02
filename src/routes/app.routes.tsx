428bb27c8ededddfcbe2f37d639add0ce73f400d
Vivian Sanchez <vivian.sanchez.dev@gmail.com>
2025-05-20 16:50:46 -0300

    refactor: add View wrapper with background color in order to prevent flickering when changing screens

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,16 +1,21 @@
+import { View } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Groups } from "@screens/Groups";
 import { Players } from "@screens/Players";
 import { NewGroup } from "@screens/NewGroup";
+import { useTheme } from "styled-components";
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
 export function AppRoutes() {
+  const { COLORS } = useTheme();
   return (
-    <Navigator screenOptions={{ headerShown: false }}>
-      <Screen name="groups" component={Groups} />
-      <Screen name="new" component={NewGroup} />
-      <Screen name="players" component={Players} />
-    </Navigator>
+    <View style={{ flex: 1, backgroundColor: COLORS.GRAY_600 }}>
+      <Navigator screenOptions={{ headerShown: false }}>
+        <Screen name="groups" component={Groups} />
+        <Screen name="new" component={NewGroup} />
+        <Screen name="players" component={Players} />
+      </Navigator>
+    </View>
   );
 }
