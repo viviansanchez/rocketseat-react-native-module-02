4c146e67fde36c3c87585e0ea41902f69efe8112
Vivian Sanchez <vivian.sanchez.dev@gmail.com>
2025-05-20 16:09:15 -0300

    feat: implement navigation

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import { Header } from "@components/header";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
@@ -6,6 +7,11 @@ import { Input } from "@components/Input";
 import { Container, Content, Icon } from "./styles";
 
 export function NewGroup() {
+  const navigation = useNavigation();
+  function handleNew() {
+    navigation.navigate("players", { group: "Rocket" });
+  }
+
   return (
     <Container>
       <Header showBackButton />
@@ -16,7 +22,7 @@ export function NewGroup() {
           subtitle="crie a turma para adicionar pessoas"
         />
         <Input placeholder="Nova turma" />
-        <Button title="Criar" style={{ marginTop: 20 }} />
+        <Button title="Criar" onPress={handleNew} style={{ marginTop: 20 }} />
       </Content>
     </Container>
   );
