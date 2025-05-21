6570eae13adff6888896fc52fccbdedc03fecc11
Vivian Sanchez <vivian.sanchez.dev@gmail.com>
2025-05-21 14:44:35 -0300

    feat: implement error cases and AppError

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { Alert } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { Header } from "@components/header";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 import { groupCreate } from "@storage/group/groupCreate";
+import { AppError } from "@utils/AppError";
 
 import { Container, Content, Icon } from "./styles";
 
@@ -14,9 +16,18 @@ export function NewGroup() {
 
   async function handleNew() {
     try {
+      if (group.trim().length === 0) {
+        return Alert.alert("Novo Grupo", "Informe o nome da turma");
+      }
+
       await groupCreate(group);
       navigation.navigate("players", { group });
     } catch (error) {
+      if (error instanceof AppError) {
+        Alert.alert("Novo grupo", error.message);
+      }
+
+      Alert.alert("Novo Grupo", "Não foi possível criar um novo grupo.");
       console.error(error);
     }
   }
